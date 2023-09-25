6ecbd32e0e071da6e4df2f4e908e972b8440fc50
Ayush <ayushpatnaik2003@gmail.com>
2023-09-25 23:41:57 +0530

    Files Added

---
+++ To Do Reminder/app/src/main/java/com/example/todoreminder/utils/model/ToDoData.kt
@@ -0,0 +1,5 @@
+package com.example.todoreminder.utils.model
+
+data class ToDoData(val taskId:String, var task:String)
+data class User(val Email: String?=null, val Name: String?=null, val Pass: String?=null)
+
